adc9653781e8bdf4388730e792b67df848c76eaf
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-28 12:29:57 -0400

    StudentInterface added

--- src/student/StudentInterface.java
+++ src/student/StudentInterface.java
@@ -19,6 +19,7 @@ public class StudentInterface {
             score = scan.nextInt();
             stu1.setScore(i, score);
         }
+        String errosmsg = stu1.validateData();
         scan.nextLine();//clears scanner buffer
         System.out.println("\n=======================\n");
         //input the second students data
@@ -26,7 +27,7 @@ public class StudentInterface {
         System.out.print("Enter name for second student: ");
         name = scan.nextLine();
         stu2.setName(name);
-        for (int i = 0; i <= 3; i++) {
+        for (int i = 1; i <= 3; i++) {
             System.out.format("Enter test score %d for %s > ", i, stu2.getName());
             score = scan.nextInt();
             stu2.setScore(i, score);
@@ -34,5 +35,9 @@ public class StudentInterface {
         System.out.println("\n=======================\n");
         System.out.println(stu1);
         System.out.println(stu2);
+        
+         
+        }
+        
     }
 }
